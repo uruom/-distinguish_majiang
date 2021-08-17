e8d8cb42884f8faada0eba2ef4936067501c3780
uruom <71624685+uruom@users.noreply.github.com>
2021-08-17 16:10:57 +0800

    Add files via upload

---
+++ maj/build/CMakeFiles/feng.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/feng.dir/opencv_1_teaching.cpp.o"
+  "feng.pdb"
+  "feng"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/feng.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ maj/build/CMakeFiles/shuang.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shuang.dir/main.cpp.o"
+  "shuang.pdb"
+  "shuang"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shuang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
