01ab51708facbf3c8436a3f6f5980a04ae605cd4
uruom <71624685+uruom@users.noreply.github.com>
2021-08-19 11:25:07 +0800

    Update main.cpp

--- maj/main.cpp
+++ maj/main.cpp
@@ -38,44 +38,44 @@ void MatchingMethod( int, void* )
   Mat img_display,templ_display;
   img.copyTo( img_display );
   templ.copyTo(templ_display);//等于是复制了一个mat，然后处理用的都是img_display，我觉得可能是避免在滑动条的时候影响到了img，导致后面的图像不对
-
+  //在这里我新增了一个templ_display，后面每次我会改变这个Mat的大小，这样可以进行大小的变化
   double minVal; double maxVal; Point minLoc; Point maxLoc;
-  double min_ans=1,max_ans=0;
+  double min_ans=1,max_ans=0;//要注意对最小值先赋值最大1，对最大值先赋值最小0
   Point matchLoc;
   Mat ans;
   int result_cols,result_rows;
-  for(int i=1;i<=10;i++)
+  //相比与之前，我把所有的定义提前，避免了在循环中出现问题
+  for(int i=1;i<=10;i++)//循环10次，每次改变0.9，也就是说最大的情况是变为原矩形的0.34倍，当然，后面这个可以根据精度什么的再调，估计会牵扯到调参的问题
   {
-   result_cols =  img.cols - templ_display.cols + 1;
-   result_rows = img.rows - templ_display.rows + 1;
-
-  result.create( result_cols, result_rows, CV_32FC1 );//zhegeyouwenti
-
-  /// 进行匹配和标准化
-  matchTemplate( img, templ_display, result, match_method );//img yuan templ pipei  result jieguotuxiang  fangfa zhiyouliuzhong
- 
-  //guiyi     
-  normalize( result, result, 0, 1, NORM_MINMAX, -1, Mat() ); 
-
-  /// 通过函数 minMaxLoc 定位最匹配的位置
+   result_cols =  img.cols - templ_display.cols + 1;//这是匹配的大小范围，简单来说能匹配的范围就是在这么大的区间以内，不然要出界报错
+   result_rows = img.rows - templ_display.rows + 1;//分别是行和列，其中row是行，col是宽
   
-  minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );//zhaodao zuixiaozhi he zuidazhi
-
-  /// 对于方法 SQDIFF 和 SQDIFF_NORMED, 越小的数值代表更高的匹配结果. 而对于其他方法, 数值越大匹配越好
+    //因为每次我的templ_display的值都会便，所以create是每次都会有一个  
+  result.create( result_cols, result_rows, CV_32FC1 );//创建矩阵
+  /*～～～～*/
+  matchTemplate( img, templ_display, result, match_method );//自带的一个匹配函数，
+  normalize( result, result, 0, 1, NORM_MINMAX, -1, Mat() ); //归一化函数，目的可以理解为把一段序列缩短至0～1中，用0～1之间的数来比较，这样方便比较最大个最小之
+  /*这两个函数一起的目的我认为是对于所匹配原图上的每一个点进行判断，就是一个对比度的判断吧，判断相似性，然后对于每个值用0～1来表示*/
+  /*～～～～*/
+  
+  minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, Mat() );//寻找result这个矩阵中的最大和最小值的位置
+  /*～～～～～这次更新最大的改动在这里，对于每一次的计算来说，都会得到一个最大值和一个最小值，那么换句话说，在所有循环中的最小值所对应的点以及缩放
+  就是这个图像的最佳匹配，也就是说对于循环中的每一次，我会记录下当前的最大/最小值，然后和已知的一个最大和最小值进行判断，大小，若是更大或者更小就
+  更新，反之则不管，这样出来以后的值就是这所有循环中最匹配的值*/
   if( match_method  == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED )
-    {   if(min_ans>minVal)
+    {   if(min_ans>minVal)//判断最小的值是否大于本次循环的最小值
        {
-          min_ans=minVal;
+          min_ans=minVal;//其中minVal是值，而其中的minLoc是所对应点的坐标
           matchLoc = minLoc;
           ans=templ_display;
        }
     }
   else
-    { if(max_ans<maxVal)
+    { if(max_ans<maxVal)//同理，是否大于最大值，不同方法找的最大或者最小值不同
       {
-        max_ans=maxVal;
+        max_ans=maxVal;////同理，maxVal是值，而其中的maxLoc是所对应点的坐标
         matchLoc = maxLoc;
-        ans=templ_display;
+        ans=templ_display;//储存当前的大小
       } 
     }
     resize(templ_display,templ_display,Size(),0.9,0.9);
