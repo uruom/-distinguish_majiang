f2b6d58c324514d16a728236fc70eec06ed95b75
uruom <71624685+uruom@users.noreply.github.com>
2021-08-24 17:51:20 +0800

    Add files via upload

---
+++ build/CMakeFiles/po.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/po.dir/main_revolve.cpp.o"
+  "po.pdb"
+  "po"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/po.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
